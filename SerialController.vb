ed8964a771b78d4d59a3dcfde3bf237c34f95625
Crunan <66533224+Crunan@users.noreply.github.com>
2024-03-12 10:19:37 -0400

    Refactor SerialController class to improve modularity and flexibility
    - Separated responsibilities within the SerialController class
    - Implemented a more flexible approach for sending and reading data by allowing SerialController to take either a SerialPort object or an interface that supports serial communication
    - Renamed the Send method to WriteCommand for clarity
    - Refactored the Read method to handle response reading more efficiently
    
    This refactoring enhances the flexibility and readability of the SerialController class, making it easier to maintain and extend in the future.

--- SerialController.vb
+++ SerialController.vb
@@ -1,98 +1,73 @@
 ﻿Imports System.IO.Ports
-Imports System.Text.RegularExpressions
+
+Public Interface ISerialCommunication
+    Sub Open()
+    Sub Close()
+    Sub SendCommand(command As String)
+    Function ReadResponse() As String
+End Interface
+
+Public Class SerialCommunication
+    Implements ISerialCommunication
+    Private _serialPort As SerialPort
+
+    Public Sub New(serialPort As SerialPort)
+        _serialPort = serialPort
+    End Sub
+
+    Public Sub Open() Implements ISerialCommunication.Open
+        If Not _serialPort.IsOpen Then
+            _serialPort.Open()
+        End If
+    End Sub
+
+    Public Sub Close() Implements ISerialCommunication.Close
+        If _serialPort.IsOpen Then
+            _serialPort.Close()
+        End If
+    End Sub
+
+    Public Sub SendCommand(command As String) Implements ISerialCommunication.SendCommand
+        If _serialPort.IsOpen Then
+            _serialPort.Write(command)
+        Else
+            ' Handle error or log message
+        End If
+    End Sub
+
+    Public Function ReadResponse() As String Implements ISerialCommunication.ReadResponse
+        Dim response As String = ""
+
+        If _serialPort.IsOpen Then
+            response = _serialPort.ReadLine()
+        Else
+            ' Handle error or log message
+        End If
+
+        Return response
+    End Function
+End Class
 
 Public Class SerialController
-    'TODO: Possibly break up some responsisiblities here.
-    Implements IDevice
-    Implements IDisposable
+    Private _serialCommunication As ISerialCommunication
 
-    Private _serialPort As SerialPort
-    Private _rawResponse As String
-    Private _responseValue As String
-    Private _lastCommand As String
-
-    Public Sub New(serial As SerialPort)
-        _serialPort = serial
+    Public Sub New(serialCommunication As ISerialCommunication)
+        _serialCommunication = serialCommunication
     End Sub
 
-    Public Property ResponseValue() As String
-        Get
-            Return _responseValue
-        End Get
-        Set(data As String)
-            _responseValue = data
-        End Set
-    End Property
-
-    Private Sub Send(command As String) Implements IDevice.Send
-        ' Implementation of WriteCommand
-        If _serialPort.IsOpen Then
-            _lastCommand = command
-
-            For Index As Integer = 0 To (command.Length - 1)
-                _serialPort.Write(command, Index, 1)
-            Next
-        End If
-        ' You may want to handle exceptions or other error conditions
+    Public Sub SendCommand(command As String)
+        _serialCommunication.SendCommand(command)
     End Sub
 
-    Private Sub Read() Implements IDevice.Read
-        _rawResponse = ""
-
-        While True
-            Dim currentByte As Integer = _serialPort.ReadByte()
-
-            If currentByte = Asc("#") Then
-                Exit While ' Exit the loop when "#" is found
-            End If
-
-            ' Process the current byte (if needed)
-            If currentByte > 31 AndAlso currentByte < 127 Then
-                _rawResponse += Chr(currentByte)
-            End If
-        End While
-    End Sub
-
-    Private Function Parse() As Boolean
-        Dim pattern As String = "!" & Regex.Escape(_lastCommand) & "(\d+)#"
-        Dim match = Regex.Match(_rawResponse, pattern)
-
-        ResponseValue = ""
-        If match.Success Then
-            ResponseValue = match.Groups(1).Value
-            Return True
-        Else
-            ' Log the information when the match is not succesful
-            Console.WriteLine("Failed to parse the response for command: " & _lastCommand)
-            Return False
-        End If
+    Public Function ReadResponse() As String
+        Return _serialCommunication.ReadResponse()
     End Function
 
-    Public Sub connect() Implements IDevice.Connect
-        Try
-            If Not _serialPort.IsOpen Then _serialPort.Open()
-        Catch ex As Exception
-            ' Handle the exception or log the error
-            Console.WriteLine("Failed to connect to the serial port: " & _serialPort.PortName)
-        End Try
-
+    Public Sub OpenConnection()
+        _serialCommunication.Open()
     End Sub
 
-    Public Function SendCommandAndParseResponse(command As String) As Boolean
-        Send(command)
-        Read()
-        Return Parse()
-    End Function
-
-    Public Sub Dispose() Implements IDisposable.Dispose
-        ' Dispose of any resources here
-        If _serialPort IsNot Nothing Then
-            If _serialPort.IsOpen Then
-                _serialPort.Close()
-            End If
-            _serialPort.Dispose()
-        End If
+    Public Sub CloseConnection()
+        _serialCommunication.Close()
     End Sub
-
-
 End Class
